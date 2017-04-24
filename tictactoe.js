0ca2081d3e7fe688fe9d6902d65327d12c14d3e2
Andrew Fechner <arfechner@gmail.com>
2017-04-24 15:15:16 -0400

    It's overdue but it works. :)

--- tictactoe.js
+++ tictactoe.js
@@ -1,25 +1,27 @@
+var prompt = require('prompt');
+
 const tictactoe = function() {
   let winner = false;
 
   let board = [[' ',' ',' '], [' ',' ',' '], [' ',' ',' ']];
 
   const checkH = function(row) {
-    if ((row[0] === row[1]) && (row[0] === row[2]) && row[0]) {
+    if ((row[0] === row[1]) && (row[0] === row[2])) {
       winner = true;
     }
   }
 
   const checkV = function(column) {
-    if ((row[0][column] === row[1][column]) && (row[0][column] === row[2][column]) && row[0][column]) {
+    if ((board[0][column] === board[1][column]) && (board[0][column] === board[2][column])) {
       winner = true;
     }
   }
 
   const checkD = function() {
-    if ((row[0][0] === row[1][1]) && (row[0][0] === row[2][2]) && row[0][0]) {
+    if ((board[0][0] === board[1][1]) && (board[0][0] === board[2][2]) && board[1][1] !== ' ') {
       winner = true;
     }
-    if ((row[0][2] === row[1][1]) && (row[0][0] === row[2][0]) && row[0][2]) {
+    if ((board[0][2] === board[1][1]) && (board[0][0] === board[2][0]) && board[1][1] !== ' ') {
       winner = true;
     }
   }
@@ -29,7 +31,7 @@ const tictactoe = function() {
     for (let i=0; i<3; i++) {
       console.log(i+'  '+board[i][0]+' | '+board[i][1]+' | '+board[i][2]);
       if (i<2) {
-        console.log('. --- --- ---');
+        console.log(' --- --- ---');
       }
     }
   }
@@ -43,42 +45,48 @@ const tictactoe = function() {
     drawBoard();
 
     const makeMove = function() {
-      console.log('Your move, player '+player+'. Please enter a row number:');
-      process.stdin.resume();
-      process.stdin.setEncoding('utf8');
-      process.stdin.once('data', function (data) {
-        row = data;
-        process.stdin.pause();
-      });
-      console.log('And a column:')
-      process.stdin.resume();
-      process.stdin.setEncoding('utf8');
-      process.stdin.once('data', function (data) {
-        col = data;
-        process.stdin.pause();
-      });
-      plays++;
-      if (board[row][col] !== ' ') {
-        console.log('Sorry, that space is taken.  Try again.');
-        makeMove();
-      } else {
-        board[row][col] = player
-        drawboard();
-        checkH();
-        checkV();
-        checkD();
-        if (winner) {
-          console.log('Congratulations, '+player+', you won!');
-          return;
-        } else {
-          if (plays === 9) {
-            console.log('Stalemate! The only winning move is not to play');
-            playGame();
-          }
-          player = player === 'X' ? 'O' : 'X';
+      console.log('Your move, player '+player+'. Please enter a row and a column.');
+      // process.stdin.resume();
+      // process.stdin.setEncoding('utf8');
+      // process.stdin.once('data', function (data) {
+      //   row = data;
+      //   process.stdin.pause();
+      // });
+
+      // console.log('And a column:')
+      // process.stdin.resume();
+      // process.stdin.setEncoding('utf8');
+      // process.stdin.once('data', function (data) {
+      //   col = data;
+      //   process.stdin.pause();
+      // });
+      prompt.start();
+      prompt.get([{name: 'row', required: true},{name: 'column', required: true}], function(err, result) {
+        row = result.row;
+        col = result.column;
+        plays++;
+        if (board[row][col] !== ' ') {
+          console.log('Sorry, that space is taken.  Try again.');
           makeMove();
+        } else {
+          board[row][col] = player;
+          drawBoard();
+          checkH(row);
+          checkV(col);
+          checkD();
+          if (winner) {
+            console.log('Congratulations, '+player+', you won!');
+            return;
+          } else {
+            if (plays === 9) {
+              console.log('Stalemate! The only winning move is not to play');
+              return;
+            }
+            player = player === 'X' ? 'O' : 'X';
+            makeMove();
+          }
         }
-      }
+      });
     };
 
     makeMove();
